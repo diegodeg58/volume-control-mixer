3d4901208ccda9081d6303d32817e2088c324dfb
Diego Rojas Torrealba <diego_deg_58@hotmail.com>
2024-09-02 19:54:43 -0400

    feat: implementa clases y modulariza

--- Project1/CAudioEndpointVolumeCallback.cpp
+++ Project1/CAudioEndpointVolumeCallback.cpp
@@ -1,1 +1,84 @@
 #include "CAudioEndpointVolumeCallback.h"
+#include <endpointvolume.h>
+#include <stdio.h>
+#include <CommCtrl.h>
+#define MAX_VOL  100
+//-----------------------------------------------------------
+// Client implementation of IAudioEndpointVolumeCallback
+// interface. When a method in the IAudioEndpointVolume
+// interface changes the volume level or muting state of the
+// endpoint device, the change initiates a call to the
+// client's IAudioEndpointVolumeCallback::OnNotify method.
+//-----------------------------------------------------------
+class CAudioEndpointVolumeCallback : public IAudioEndpointVolumeCallback
+{
+    LONG _cRef;
+    const HWND hWndFader;
+    const HWND hWndTextFader;
+
+public:
+    CAudioEndpointVolumeCallback(HWND hWndFader, HWND hWndTextFader) :
+        _cRef(1), hWndFader(hWndFader), hWndTextFader(hWndTextFader)
+    {
+    }
+
+    ~CAudioEndpointVolumeCallback()
+    {
+        delete this;
+    }
+
+    // IUnknown methods -- AddRef, Release, and QueryInterface
+
+    ULONG STDMETHODCALLTYPE AddRef()
+    {
+        return InterlockedIncrement(&_cRef);
+    }
+
+    ULONG STDMETHODCALLTYPE Release()
+    {
+        ULONG ulRef = InterlockedDecrement(&_cRef);
+        if (0 == ulRef)
+        {
+            delete this;
+        }
+        return ulRef;
+    }
+
+    HRESULT STDMETHODCALLTYPE QueryInterface(REFIID riid, VOID** ppvInterface)
+    {
+        if (IID_IUnknown == riid)
+        {
+            AddRef();
+            *ppvInterface = (IUnknown*)this;
+        }
+        else if (__uuidof(IAudioEndpointVolumeCallback) == riid)
+        {
+            AddRef();
+            *ppvInterface = (IAudioEndpointVolumeCallback*)this;
+        }
+        else
+        {
+            *ppvInterface = NULL;
+            return E_NOINTERFACE;
+        }
+        return S_OK;
+    }
+
+    // Callback method for endpoint-volume-change notifications.
+
+    HRESULT STDMETHODCALLTYPE OnNotify(PAUDIO_VOLUME_NOTIFICATION_DATA pNotify)
+    {
+        if (pNotify == NULL)
+        {
+            return E_INVALIDARG;
+        }
+
+        float currentVolume = pNotify->fMasterVolume;
+        char text[4];
+        sprintf_s(text, "%d", (int)(currentVolume * MAX_VOL));
+        PostMessageA(hWndFader, TBM_SETPOS, TRUE, LPARAM((UINT32)(MAX_VOL - currentVolume * MAX_VOL)));
+        SetWindowTextA(hWndTextFader, (LPCSTR)text);
+
+        return S_OK;
+    }
+};
--- Project1/MainWindow.cpp
+++ Project1/MainWindow.cpp
@@ -4,203 +4,263 @@
 #include <mmdeviceapi.h>
 #include <endpointvolume.h>
 #include <functiondiscoverykeys_devpkey.h>
-#include "CAudioEndpointVolumeCallback.h"
+#include "CAudioEndpointVolumeCallback.cpp"
 #define MAX_LOADSTRING 100
 
 class MainWindow {
-    HINSTANCE hInst;                                // current instance
-    WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
-    WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
+public:
 
-    HWND* fader;
-    HWND* textFader;
-    IMMDevice** iEndpointDevice;
-    IAudioEndpointVolume** iEndpointVolume;
-    CAudioEndpointVolumeCallback** ppCAudioEndpointVolumeCallback;
+	MainWindow(HINSTANCE hInstance) {
+		hInst = hInstance;
+		LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+		LoadStringW(hInstance, IDC_PROJECT1, szWindowClass, MAX_LOADSTRING);
+		MyRegisterClass(hInstance);
+	}
 
-    UINT countDevices = NULL;
+	HWND CreateMainWindow() {
+		return CreateWindowExW(
+			0L, 
+			szWindowClass, 
+			szTitle, 
+			0x00020000L | 0x00080000L, 
+			((int)0x80000000), 
+			0, 
+			((int)0x80000000), 
+			0, 
+			nullptr, 
+			nullptr, 
+			hInst, 
+			this);
+	}
 
-    ATOM MyRegisterClass(HINSTANCE hInstance)
-    {
-        WNDCLASSEXW wcex;
+private:
 
-        wcex.cbSize = sizeof(WNDCLASSEX);
+	HINSTANCE hInst;                                // current instance
+	WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
+	WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
+	HWND hWnd;
 
-        wcex.style = CS_HREDRAW | CS_VREDRAW;
-        wcex.lpfnWndProc = sWndProc;
-        wcex.cbClsExtra = 0;
-        wcex.cbWndExtra = 0;
-        wcex.hInstance = hInstance;
-        wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_PROJECT1));
-        wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
-        wcex.hbrBackground = (HBRUSH)(COLOR_MENU + 1);
-        wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_PROJECT1);
-        wcex.lpszClassName = szWindowClass;
-        wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+	HWND* fader;
+	HWND* textFader;
+	IMMDevice** iEndpointDevice;
+	IAudioEndpointVolume** iEndpointVolume;
+	CAudioEndpointVolumeCallback** ppCAudioEndpointVolumeCallback;
+	UINT countDevices = NULL;
 
-        return RegisterClassExW(&wcex);
-    }
+	ATOM MyRegisterClass(HINSTANCE hInstance)
+	{
+		WNDCLASSEXW wcex;
 
-    static LRESULT CALLBACK sWndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
-        MainWindow* pMainWindow = reinterpret_cast<MainWindow*>(GetWindowLongPtr(hWnd, GWLP_USERDATA));
-        if (pMainWindow) return pMainWindow->WndProc(hWnd, message, wParam, lParam);
-        return DefWindowProc(hWnd, message, wParam, lParam);
-    }
+		wcex.cbSize = sizeof(WNDCLASSEX);
 
-    static LRESULT CALLBACK sAbout(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
-        MainWindow* pMainWindow = reinterpret_cast<MainWindow*>(GetWindowLongPtr(hWnd, GWLP_USERDATA));
-        if (pMainWindow) return pMainWindow->About(hWnd, message, wParam, lParam);
-        return DefWindowProc(hWnd, message, wParam, lParam);
-    }
+		wcex.style = CS_HREDRAW | CS_VREDRAW;
+		wcex.lpfnWndProc = sWndProc;
+		wcex.cbClsExtra = 0;
+		wcex.cbWndExtra = 0;
+		wcex.hInstance = hInstance;
+		wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_PROJECT1));
+		wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+		wcex.hbrBackground = (HBRUSH)(COLOR_MENU + 1);
+		wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_PROJECT1);
+		wcex.lpszClassName = szWindowClass;
+		wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
 
-    LRESULT WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
-    {
-        switch (message) {
+		return RegisterClassExW(&wcex);
+	}
 
-        case WM_COMMAND:
-        {
-            int wmId = LOWORD(wParam);
-            // Parse the menu selections:
-            switch (wmId)
-            {
-            case IDM_ABOUT:
-                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, sAbout);
-                break;
-            case IDM_EXIT:
-                DestroyWindow(hWnd);
-                break;
-            default:
-                return DefWindowProc(hWnd, message, wParam, lParam);
-            }
-        }
-        break;
-        case WM_PAINT:
-        {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hWnd, &ps);
-            // TODO: Add any drawing code that uses hdc here...
-            EndPaint(hWnd, &ps);
-        }
-        break;
-        case WM_SIZE: {
-            RECT rect;
-            GetWindowRect(hWnd, &rect);
-            break;
-        }
-        case WM_CREATE: {
-            CreateSliders(hWnd);
-            break;
-        }
-        case WM_VSCROLL: {
-            if (LOWORD(wParam) == TB_THUMBPOSITION || LOWORD(wParam) == TB_THUMBTRACK) {
-                int value = (int)MAX_VOL - HIWORD(wParam);
-                char text[5];
-                sprintf_s(text, "%d", value);
+	static LRESULT CALLBACK sWndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+		MainWindow* pMainWindow;
+		if (message == WM_NCCREATE) {
+			LPCREATESTRUCT lpcs = reinterpret_cast<LPCREATESTRUCT>(lParam);
+			pMainWindow = static_cast<MainWindow*>(lpcs->lpCreateParams);
+			pMainWindow->hWnd = hWnd;
+			SetWindowLongPtrW(hWnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(pMainWindow));
+		}
+		else {
+			pMainWindow = reinterpret_cast<MainWindow*>(GetWindowLongPtr(hWnd, GWLP_USERDATA));
+		}
+		if (pMainWindow)
+			return pMainWindow->WndProc(message, wParam, lParam);
+		return false;
+	}
 
-                char textControl[20];
-                for (int i = 0; i < countDevices; i++) {
-                    if (fader[i] == (HWND)lParam) {
-                        sprintf_s(textControl, "%d", i);
-                        float newValue = (float)(value) / MAX_VOL;
-                        iEndpointVolume[i]->SetMasterVolumeLevelScalar(newValue, NULL);
-                        SetWindowTextA(textFader[i], (LPCSTR)text);
-                    }
-                }
-            }
-            break;
-        }
-        case WM_DESTROY:
-            CoUninitialize();
-            for (int i = 0; i < countDevices; i++) {
-                iEndpointVolume[i]->UnregisterControlChangeNotify(ppCAudioEndpointVolumeCallback[i]);
-            }
-            delete[] fader;
-            delete[] textFader;
-            delete[] iEndpointVolume;
-            delete[] iEndpointDevice;
-            delete[] ppCAudioEndpointVolumeCallback;
-            PostQuitMessage(0);
-            break;
-        default:
-            return DefWindowProc(hWnd, message, wParam, lParam);
-        }
-        return 0;
-    }
+	static LRESULT CALLBACK sAbout(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+		MainWindow* pMainWindow;
+		if (message == WM_INITDIALOG) {
+			pMainWindow = reinterpret_cast<MainWindow*>(lParam);
+			SetWindowLongPtrW(hWnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(pMainWindow));
+		}
+		else {
+			pMainWindow = reinterpret_cast<MainWindow*>(GetWindowLongPtr(hWnd, GWLP_USERDATA));
+		}
+		if (pMainWindow)
+			return pMainWindow->About(hWnd, message, wParam, lParam);
+		return false;
+	}
 
-    INT_PTR About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
-    {
-        UNREFERENCED_PARAMETER(lParam);
-        switch (message)
-        {
-        case WM_INITDIALOG:
-            return (INT_PTR)TRUE;
+    LRESULT WndProc(UINT message, WPARAM wParam, LPARAM lParam)
+	{
+		switch (message) {
 
-        case WM_COMMAND:
-            if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-            {
-                EndDialog(hDlg, LOWORD(wParam));
-                return (INT_PTR)TRUE;
-            }
-            break;
-        }
-        return (INT_PTR)FALSE;
-    }
+		case WM_COMMAND:
+		{
+			int wmId = LOWORD(wParam);
+			// Parse the menu selections:
+			switch (wmId)
+			{
+			case IDM_ABOUT:
+				DialogBoxParam(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, sAbout, (LPARAM) this);
+				break;
+			case IDM_EXIT:
+				DestroyWindow(hWnd);
+				break;
+			default:
+				return DefWindowProc(hWnd, message, wParam, lParam);
+			}
+		}
+		break;
+		case WM_PAINT:
+		{
+			PAINTSTRUCT ps;
+			HDC hdc = BeginPaint(hWnd, &ps);
+			// TODO: Add any drawing code that uses hdc here...
+			EndPaint(hWnd, &ps);
+		}
+		break;
+		case WM_SIZE: {
+			RECT rect;
+			GetWindowRect(hWnd, &rect);
+			break;
+		}
+		case WM_CREATE: {
+			CreateSliders();
+			break;
+		}
+		case WM_VSCROLL: {
+			if (LOWORD(wParam) == TB_THUMBPOSITION || LOWORD(wParam) == TB_THUMBTRACK) {
+				int value = (int)MAX_VOL - HIWORD(wParam);
+				char text[5];
+				sprintf_s(text, "%d", value);
 
-    BOOL CreateSliders(HWND hWnd) {
-        HRESULT hr;
-        hr = CoInitialize(NULL);
-        IMMDeviceEnumerator* deviceEnumerator = NULL;
-        hr = CoCreateInstance(__uuidof(MMDeviceEnumerator), NULL, CLSCTX_INPROC_SERVER, __uuidof(IMMDeviceEnumerator), (LPVOID*)&deviceEnumerator);
+				char textControl[20];
+				for (unsigned int i = 0; i < countDevices; i++) {
+					if (fader[i] == (HWND)lParam) {
+						sprintf_s(textControl, "%d", i);
+						float newValue = (float)(value) / MAX_VOL;
+						iEndpointVolume[i]->SetMasterVolumeLevelScalar(newValue, NULL);
+						SetWindowTextA(textFader[i], (LPCSTR)text);
+					}
+				}
+			}
+			break;
+		}
+		case WM_DESTROY: {
+			CoUninitialize();
+			for (int i = 0; i < countDevices; i++) {
+				iEndpointVolume[i]->UnregisterControlChangeNotify(ppCAudioEndpointVolumeCallback[i]);
+			}
+			delete[] fader;
+			delete[] textFader;
+			delete[] iEndpointVolume;
+			delete[] iEndpointDevice;
+			delete[] ppCAudioEndpointVolumeCallback;
+			PostQuitMessage(0);
+			break;
+		}
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+		}
+		return 0;
+	}
 
-        IMMDeviceCollection* deviceCollection = NULL;
-        hr = deviceEnumerator->EnumAudioEndpoints(eRender, DEVICE_STATE_ACTIVE, &deviceCollection);
+	INT_PTR About(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+	{
+		UNREFERENCED_PARAMETER(lParam);
+		switch (message)
+		{
+		case WM_INITDIALOG:
+			return (INT_PTR)TRUE;
 
-        deviceEnumerator->Release();
-        deviceEnumerator = NULL;
+		case WM_COMMAND:
+			if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+			{
+				EndDialog(hWnd, LOWORD(wParam));
+				return (INT_PTR)TRUE;
+			}
+			break;
+		}
+		return (INT_PTR)FALSE;
+	}
 
-        hr = deviceCollection->GetCount(&countDevices);
-        fader = new HWND[countDevices];
-        textFader = new HWND[countDevices];
-        iEndpointDevice = new IMMDevice * [countDevices];
-        iEndpointVolume = new IAudioEndpointVolume * [countDevices];
-        ppCAudioEndpointVolumeCallback = new CAudioEndpointVolumeCallback * [countDevices];
+	BOOL CreateSliders() {
+		HRESULT hr;
+		hr = CoInitialize(NULL);
+		IMMDeviceEnumerator* deviceEnumerator = NULL;
+		hr = CoCreateInstance(
+			__uuidof(MMDeviceEnumerator), 
+			NULL, 
+			CLSCTX_INPROC_SERVER, 
+			__uuidof(IMMDeviceEnumerator), 
+			(LPVOID*)&deviceEnumerator);
 
-        int x, y = 480;
+		IMMDeviceCollection* deviceCollection = NULL;
+		hr = deviceEnumerator->EnumAudioEndpoints(eRender, DEVICE_STATE_ACTIVE, &deviceCollection);
 
-        for (ULONG i = 0; i < countDevices; i++) {
-            fader[i] = CreateWindowEx(0, TRACKBAR_CLASS, NULL, WS_CHILD | WS_VISIBLE | WS_TABSTOP | TBS_VERT | TBS_BOTH, 10 + i * 150, 10, 50, 300, hWnd, NULL, hInst, NULL);
-            textFader[i] = CreateWindowEx(0, L"Static", NULL, WS_CHILD | WS_VISIBLE, 25 + i * 150, 310, 50, 30, hWnd, NULL, hInst, NULL);
-            HWND hTextDevice = CreateWindowEx(0, L"Static", NULL, WS_CHILD | WS_VISIBLE, 25 + i * 150, 340, 100, 100, hWnd, NULL, hInst, NULL);
+		deviceEnumerator->Release();
+		deviceEnumerator = NULL;
 
-            hr = deviceCollection->Item(i, &iEndpointDevice[i]);
-            hr = iEndpointDevice[i]->Activate(__uuidof(IAudioEndpointVolume), CLSCTX_INPROC_SERVER, NULL, (LPVOID*)&iEndpointVolume[i]);
+		hr = deviceCollection->GetCount(&countDevices);
+		fader = new HWND[countDevices];
+		textFader = new HWND[countDevices];
+		iEndpointDevice = new IMMDevice * [countDevices];
+		iEndpointVolume = new IAudioEndpointVolume * [countDevices];
+		ppCAudioEndpointVolumeCallback = new CAudioEndpointVolumeCallback * [countDevices];
 
-            ppCAudioEndpointVolumeCallback[i] = new CAudioEndpointVolumeCallback(fader[i], textFader[i]);
-            hr = iEndpointVolume[i]->RegisterControlChangeNotify(ppCAudioEndpointVolumeCallback[i]);
+		int x, y = 480;
 
-            float currentVolume;
-            hr = iEndpointVolume[i]->GetMasterVolumeLevelScalar(&currentVolume);
-            char text[4];
-            sprintf_s(text, "%d", (int)(currentVolume * MAX_VOL));
-            SendMessageA(fader[i], TBM_SETPOS, TRUE, (int)MAX_VOL - (currentVolume * MAX_VOL));
-            SetWindowTextA(textFader[i], (LPCSTR)text);
+		for (ULONG i = 0; i < countDevices; i++) {
+			fader[i] = CreateWindowEx(
+				0, TRACKBAR_CLASS, NULL, 
+				WS_CHILD | WS_VISIBLE | WS_TABSTOP | TBS_VERT | TBS_BOTH, 
+				10 + i * 150, 10, 50, 300, hWnd, NULL, hInst, NULL);
+			textFader[i] = CreateWindowEx(
+				0, L"Static", 
+				NULL, WS_CHILD | WS_VISIBLE, 
+				25 + i * 150, 310, 50, 30, hWnd, NULL, hInst, NULL);
+			HWND hTextDevice = CreateWindowEx(
+				0, L"Static", NULL, 
+				WS_CHILD | WS_VISIBLE, 
+				25 + i * 150, 340, 100, 100, hWnd, NULL, hInst, NULL);
 
-            IPropertyStore* pProps = NULL;
-            hr = iEndpointDevice[i]->OpenPropertyStore(STGM_READ, &pProps);
+			hr = deviceCollection->Item(i, &iEndpointDevice[i]);
+			hr = iEndpointDevice[i]->Activate(__uuidof(IAudioEndpointVolume), CLSCTX_INPROC_SERVER, NULL, (LPVOID*)&iEndpointVolume[i]);
 
-            PROPVARIANT varName;
-            PropVariantInit(&varName);
-            hr = pProps->GetValue(PKEY_Device_FriendlyName, &varName);
-            char textDevice[100];
-            sprintf_s(textDevice, "%S", varName.pwszVal);
-            SetWindowTextA(hTextDevice, (LPCSTR)textDevice);
+			ppCAudioEndpointVolumeCallback[i] = new CAudioEndpointVolumeCallback(fader[i], textFader[i]);
+			hr = iEndpointVolume[i]->RegisterControlChangeNotify(ppCAudioEndpointVolumeCallback[i]);
 
-            x = 150 + i * 150;
-        }
-        RECT rect;
-        GetWindowRect(hWnd, &rect);
-        MoveWindow(hWnd, (int)rect.left, (int)rect.top, x, y, true);
+			float currentVolume;
+			hr = iEndpointVolume[i]->GetMasterVolumeLevelScalar(&currentVolume);
+			char text[4];
+			sprintf_s(text, "%d", (int)(currentVolume * MAX_VOL));
+			SendMessageA(fader[i], TBM_SETPOS, TRUE, (int)MAX_VOL - (currentVolume * MAX_VOL));
+			SetWindowTextA(textFader[i], (LPCSTR)text);
 
-        return TRUE;
-    }
+			IPropertyStore* pProps = NULL;
+			hr = iEndpointDevice[i]->OpenPropertyStore(STGM_READ, &pProps);
+
+			PROPVARIANT varName;
+			PropVariantInit(&varName);
+			hr = pProps->GetValue(PKEY_Device_FriendlyName, &varName);
+			char textDevice[100];
+			sprintf_s(textDevice, "%S", varName.pwszVal);
+			SetWindowTextA(hTextDevice, (LPCSTR)textDevice);
+
+			x = 150 + i * 150;
+		}
+		RECT rect;
+		GetWindowRect(hWnd, &rect);
+		MoveWindow(hWnd, (int)rect.left, (int)rect.top, x, y, true);
+
+		return TRUE;
+	}
 };
